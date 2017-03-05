c1427186a6ebd3c345cd87b165faea340da9c40c
David Ladowitz <david@ladowitz.com>
2017-03-05 12:30:32 -0800

    initial commit

---
+++ conditionals.R
@@ -0,0 +1,17 @@
+x <- 1
+
+if (x == 1){
+  print("X is equal to one!")
+}
+
+if ( (x %% 2) == 0 ){
+  print("X is even")
+} else {
+  print("X is Not even")
+}
+
+if (is.matrix(x) == TRUE){
+  print("X is a Matrix")
+} else {
+  print("X is NOT a Matrix")
+}
